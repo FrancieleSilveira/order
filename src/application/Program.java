38530759f0242524c554c91f5f62b3790d32cfb7
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-20 00:17:43 -0300

    Program finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,15 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
 
 public class Program {
 
@@ -16,11 +21,47 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Client cli = new Client("Franciele", "franciele@email.com", sdf.parse("16/03/1992"));
+		System.out.println("Enter client data:");
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+		System.out.println("Email: ");
+		String email = sc.next();
+		System.out.println("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next()); //.parse transformará a String recebida em Date
 		
-		System.out.println(cli);
-
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("\nEnter order data:");
+		System.out.println("Status: ");
+		//.valueOf converte o que for recebido em String para o valor correspondente definido no OrderStatus
+		OrderStatus status = OrderStatus.valueOf(sc.next()); 
+		
+		Order order = new Order(new Date(), status, client);
+		
+		System.out.println("How many items to this item? ");
+		int N = sc.nextInt();
+		for (int i=0; i<N; i++) {
+			System.out.println("Enter #" + (i+1) + " item data: ");
+			System.out.println("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.println("Product price: ");
+			double productPrice = sc.nextDouble();
+			System.out.println("Quantity: ");
+			int quantity = sc.nextInt();
+			
+			Product product = new Product(productName, productPrice);
+			
+			OrderItem oi = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(oi);
+		}
+		
+		System.out.println();
+		System.out.println(order);
+		
 		sc.close();
+		
 	}
 
 }
