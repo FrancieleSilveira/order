78a0216a91506d7d79f09d6f2ee472de07bf7364
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-19 22:54:19 -0300

    Primeiro teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,21 @@
 package application;
 
+import java.util.Locale;
+
+import entities.OrderItem;
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Locale.setDefault(Locale.US);
+		
+		Product p = new Product("TV", 1000.0);
+		
+		OrderItem oi1 = new OrderItem(1, 1000.0, p);
+		
+		System.out.println(oi1);
 
 	}
 
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,20 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
 public class Order {
+	
+	private Date moment;
+	private OrderStatus status;
+	private List<OrderItem> items = new ArrayList<>();
+	
+	public Order() {
+	}
+	
+	
 
 }
