7b300ea942146308f0868a7856fc2d5ba198256a
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-19 23:48:33 -0300

    Métodos criados em Order

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,13 +9,84 @@ import entities.enums.OrderStatus;
 
 public class Order {
 	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
 	private Date moment;
 	private OrderStatus status;
-	private List<OrderItem> items = new ArrayList<>();
+	//uma ordem tem um cliente
+	private Client client;
+	//uma ordem tem uma lista de itens
+	private List<OrderItem> items = new ArrayList<OrderItem>();
 	
 	public Order() {
 	}
+
+	//no construtor não inclui a List
+	//pois o Order já recebeu uma lista vazia para ser adicionado itens
+	//para isso será utilizado um método específico para essa tarefa
+	public Order(Date moment, OrderStatus status, Client client) {
+		super();
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
 	
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
 	
+	public double total() {
+		double sum = 0.0;
+		for (OrderItem i : items) {
+			sum += i.subTotal();
+		}
+		return sum;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ") ;
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : items) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
+	}
 
 }
